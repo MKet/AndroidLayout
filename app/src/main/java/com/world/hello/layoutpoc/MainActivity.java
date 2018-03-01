3a1c4f49020c2923d25358b41f689b6287955c12
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2018-03-01 13:32:03 +0100

    add navigation

--- app/src/main/java/com/world/hello/layoutpoc/MainActivity.java
+++ app/src/main/java/com/world/hello/layoutpoc/MainActivity.java
@@ -1,7 +1,9 @@
 package com.world.hello.layoutpoc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tableButton = findViewById(R.id.TableButton);
+        Button gridButton = findViewById(R.id.GridButton);
+
+        tableButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Table.class);
+            startActivity(intent);
+        });
+
+        gridButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, GridActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
